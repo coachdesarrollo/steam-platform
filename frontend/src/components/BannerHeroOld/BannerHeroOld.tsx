fa468d54dc3ff8c464a0b5cc03cde0fee8b94562
Cesar Fajardo <cesarjavierfajardoortiz@gmail.com>
2024-07-12 20:38:05 -0500

    ajuste

--- frontend/src/components/BannerHeroOld/BannerHeroOld.tsx
+++ frontend/src/components/BannerHeroOld/BannerHeroOld.tsx
@@ -24,7 +24,7 @@ const BannerHero: React.FC = () => {
                 </div>
                 <input type="email" id="default-email" className="block w-full p-4 sm:ps-10 text-sm text-gray-900 border border-gray-300 rounded-md bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-neutral-900/70 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required/>
                 <button type="submit" className="w-2/5 flex justify-evenly items-center gap-3 text-black bg-yellow-500/80 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-neutral-900 font-semibold rounded-md text-xl/none p-4 dark:bg-yellow-400 dark:hover:bg-yelllow-400 dark:focus:ring-yellow-500">
-                    Comenzar <ArrowIcon className="h-5 text-neutral-800" />
+                    {/* Comenzar <ArrowIcon className="h-5 text-neutral-800" /> */}
                 </button>
             </div>
         </form>
