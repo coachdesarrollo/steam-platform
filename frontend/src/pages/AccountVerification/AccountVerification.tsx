f3ce2e34b1a06b0689dae59025a3e3dbd929f035
Cesar Fajardo <cesarjavierfajardoortiz@gmail.com>
2024-07-17 00:56:29 -0500

    Vista de verificacion de codigo

--- frontend/src/pages/AccountVerification/AccountVerification.tsx
+++ frontend/src/pages/AccountVerification/AccountVerification.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/Button";
+import { ButtonSize, ButtonStyle, ButtonTheme } from "@/config/themes/button";
+import { Link } from "react-router-dom";
+
 type VerificationButtonProps = Pick<
     React.ButtonHTMLAttributes<HTMLButtonElement>,
     "type" | "className" | "onClick" | "disabled" | "children"
@@ -6,8 +10,8 @@ type VerificationButtonProps = Pick<
 const VerificationButton: React.FC<VerificationButtonProps> = ({
     type = "button", onClick, className, children, disabled
 }) => {
-    const sharedClassName = "inline-block tracking-wider font-normal rounded cursor-pointer transition duration-300 ease-in-out px-4 py-2";
-    const buttonClassName = sharedClassName.concat(sharedClassName);
+    const sharedClassName = "inline-block shadow border-2 rounded-md cursor-pointer transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg";
+    const buttonClassName = className ? `${sharedClassName} ${className}` : sharedClassName;
     return (
         <button
             type={type}
@@ -21,42 +25,41 @@ const VerificationButton: React.FC<VerificationButtonProps> = ({
 };
 
 const AccountVerification: React.FC = () => {
+    const sharedClassName = "w-16 sm:flex-1 border-2 border-[#615D5D] grid place-content-center";
     return (
-        <div className="container mx-auto px-4 h-full">
-            <div className="flex content-center items-center justify-center h-full">
-                <div className="w-full lg:w-6/12 px-4">
-                    <div className="relative flex flex-col min-w-0 break-words w-full mb-4 shadow-lg rounded-lg bg-blueGray-200 border-0">
-                        <div className="rounded-t mb-0 px-6 py-6">
-                            <div className="text-center mb-3">
-                                <h2 className="text-blueGray-500 text-sm font-bold">
-                                ¡ExceIente! Ahora verifiquemos tu email
-                                </h2>
-                                <p className="text-blueGray-700 text-xl font-bold">
-                                    Hemos enviado un número de confirmación a loremipsum@gamil.com
-                                    hará completar la verificación.
-                                </p>
-                                <p className="text-blueGray-700 text-xl font-bold">
-                                    Digite el codigo de verificación
-                                </p>
-                                <div className="flex flex-row flex-wrap border border-neutral-200 font-extrabold">
-                                    <div className="p-5">2</div>
-                                    <div className="p-5">0</div>
-                                    <div className="p-5">2</div>
-                                    <div className="p-5">4</div>
-                                    <div className="p-5">1</div>
-                                </div>
-                            </div>
-                            <VerificationButton className="text-black bg-yellow-400 hover:bg-yellow-500">
-                                Volver a generar
-                            </VerificationButton>
-                            <VerificationButton className="text-yellow-400 bg-black hover:text-black hover:bg-yellow-500">
-                                Listo
-                            </VerificationButton>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <main className="grid place-content-center h-screen bg-banner-hero bg-no-repeat bg-cover bg-center bg-black/70 bg-blend-multiply">
+            <form className="bg-black/60 grid gap-12 text-center px-6 sm:px-12 py-8">
+                <section className="max-w-sm sm:max-w-sm grid place-content-center">
+                    <span className="text-sm text-white/50">3 de 4</span>
+                    <h2 className="text-4xl text-white font-bold mb-6">¡Excelente! Ahora verifiquemos tu email</h2>
+                    <p className="text-base text-white">Hemos enviado un número de confirmación a <strong>loremipsum@gamil.com</strong> ára completar la verificación.</p>
+                </section>
+                <section className="flex flex-col flex-wrap gap-y-2">
+                    <p className="text-base font-medium text-white/50">Digite el codigo de verificación</p>
+                    <section className="h-16 sm:h-[4.8rem] flex items-stretch justify-center font-extrabold text-4xl text-white">
+                        <div className={sharedClassName}>2</div>
+                        <div className={sharedClassName}>0</div>
+                        <div className={sharedClassName}>2</div>
+                        <div className={sharedClassName}>4</div>
+                        <div className={sharedClassName}>1</div>
+                    </section>
+                </section>
+                <section className="flex flex-col flex-wrap items-stretch justify-center px-8 sm:px-0 gap-y-3">
+                    <VerificationButton className="text-black border-[#f7df1e] bg-[#f7df1e] hover:bg-yellow-400 hover:border-yellow-400 text-xl font-semibold px-4 py-2">
+                        Volver a generar
+                    </VerificationButton>
+                    <VerificationButton className="border-[#f7df1e] text-[#f7df1e] bg-transparent hover:text-black hover:border-yellow-400 hover:bg-yellow-400 text-xl font-semibold px-4 py-2">
+                        Listo
+                    </VerificationButton>
+                    <Link
+                        to={"register/personal-data"}
+                        className="text-sm text-center text-[#928f8f] hover:text-white/75 font-medium tracking-wide"
+                    >
+                        Omitir
+                    </Link>
+                </section>
+            </form>
+        </main>
     );
 };
 
