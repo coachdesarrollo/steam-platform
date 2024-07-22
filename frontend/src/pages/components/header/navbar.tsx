27a5fc9dee720d19d098c95073bb7316c9f923e5
CardManj <c.manjarres2003@gmail.com>
2024-07-22 11:23:26 -0500

    corrigiendo responsive del landing navbar

--- frontend/src/pages/components/header/navbar.tsx
+++ frontend/src/pages/components/header/navbar.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@/common/components/buttons";
 import { ButtonSize, ButtonStyle } from "@/common/config/themes/button";
 import { Link } from "@tanstack/react-router";
-import logo from "/assets/imagotiposteamwhite.png";
+import logoLarge from "/assets/imagotiposteamwhite.png";
+import logoTiny from "/assets/light-logo.png";
 
 export function NavBar() {
   return (
-    <nav className="bg-transparent text-white px-20 sm:px-14 pt-0 flex flex-col sm:flex-row justify-between items-center w-full z-10">
-      <div className="flex items-center space-x-4 mb-4 sm:mb-0">
-        <img className="max-w-xs" src={logo} alt="SteamAcademy" />
+    <nav className="bg-transparent text-white px-20 sm:px-14 pt-0 flex  sm:flex-row justify-between items-center w-full z-10 max-sm:px-8">
+      <div //
+       className="flex justify-center items-center space-x-4 mb-4 sm:mb-0 ">
+        <img className="max-w-xs hidden sm:block" src={logoLarge} alt="SteamAcademy" />
+        <img className="max-w-xs block sm:hidden h-10" src={logoTiny} alt="SteamAcademy" />
       </div>
       <Link to="/login">
         <Button
