4cbaa1e1a0e21005e507752ee4322ff48a1984af
= <coachsdesarrollo@gmail.com>
2024-07-23 13:39:27 -0500

    mejorando faq component

--- frontend/src/pages/components/faq/question.tsx
+++ frontend/src/pages/components/faq/question.tsx
@@ -7,16 +7,16 @@ interface QuestionProps {
 
 export function Question({ answer, question }: QuestionProps) {
   return (
-    <details className="text-[#EBEBEB] mx-0">
-      <summary className="cursor-pointer flex justify-between items-center bg-[#2D2D2D] p-4 hover:bg-[#424141] rounded-sm transition-colors">
-        <h2 className="font-semibold cursor-pointer text-[clamp(1rem,4vw,1.5rem)] max-sm:text-[clamp(1rem,3vw,1.3rem)]">
+    <details className="text-[#EBEBEB] group">
+      <summary className="cursor-pointer flex justify-between items-center bg-[#2D2D2D] p-4 hover:bg-[#424141] rounded-sm transition duration-500">
+        <h2 className="font-semibold cursor-pointer group-open:animate-small-text animate-big-text">
           {question}
         </h2>
-        <span>
+        <span className="group-open:rotate-45 transition duration-300">
           <PlusIcon />
         </span>
       </summary>
-      <div className="bg-[#3f3f3f] p-4 text-[clamp(1rem,3vw,1.2rem)]">
+      <div className="bg-[#3f3f3f] p-4 text-[clamp(1rem,3vw,1.2rem)] leading-10 text-pretty text-xl text-white/80">
         {answer}
       </div>
     </details>
