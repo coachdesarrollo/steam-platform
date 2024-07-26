d0cd9235afc81da3cd34901e2d913f73f27f7a21
= <coachsdesarrollo@gmail.com>
2024-07-26 14:50:25 -0500

    añadiendo nuevos iconos

---
+++ frontend/src/common/components/icons/x.icon.tsx
@@ -0,0 +1,11 @@
+export function XIcon() {
+  return (
+    <svg className="size-4" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24">
+      <path
+        d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
