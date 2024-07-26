e0ea5ff01203f4d872c4b727635d9f5aeaa2630c
= <coachsdesarrollo@gmail.com>
2024-07-26 14:50:45 -0500

    mejorando los errores del input

--- frontend/src/pages/register/_layout/access-credential/schemas/index.ts
+++ frontend/src/pages/register/_layout/access-credential/schemas/index.ts
@@ -1,26 +1,25 @@
 import { z } from "zod";
 
-export const EmailSchema = z.string().email({ message: "Debe ser un correo electrónico válido" });
+export const EmailSchema = z
+  .string({ required_error: "Este campo es requerido" })
+  .email({ message: "No es un correo válido" });
 
 export const UsernameSchema = z
   .string()
-  .min(3, { message: "El nombre de usuario debe tener al menos 3 caracteres" })
-  .max(12, { message: "El nombre de usuario no puede tener más de 12 caracteres" });
+  .min(3, { message: "Minimo 3 caracteres" })
+  .max(12, { message: "Maximo 12 caracteres" });
 
 export const PasswordSchema = z
-  .string()
-  .min(8, { message: "La contraseña debe tener al menos 8 caracteres" })
-  .regex(/^(?=.*[a-z])/, { message: "La contraseña debe contener al menos una letra minúscula" })
-  .regex(/^(?=.*[A-Z])/, { message: "La contraseña debe contener al menos una letra mayúscula" })
-  .regex(/^(?=.*\d)/, { message: "La contraseña debe contener al menos un número" })
+  .string({ required_error: "Este campo es requerido" })
+  .min(8, { message: "Minimo 8 caracteres" })
+  .regex(/^(?=.*[a-z])/, { message: "Minimo una letra en minúscula" })
+  .regex(/^(?=.*[A-Z])/, { message: "Minimo una letra en mayúscula" })
+  .regex(/^(?=.*\d)/, { message: "Minimo un numero" })
   .regex(/^(?=.*[!#$%&'()*+\,\-\./:;<=>?@[\]^_`{|}~@$!%*?&_-])/, {
-    message: "La contraseña debe contener al menos un carácter especial",
+    message: "Minimo un caracter especial",
   });
 
-export const ProfileSchema = z
-  .string()
-  .max(255, { message: "El perfil no puede superar los 255 caracteres" })
-  .optional();
+export const ProfileSchema = z.string().max(255).optional();
 
 export const AccessCredentialSchema = z.object({
   email: EmailSchema,
