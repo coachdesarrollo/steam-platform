f88c00641c0071f1a4d48cd925ac1ceaf182fa8d
CardManj <c.manjarres2003@gmail.com>
2024-07-30 11:44:46 -0500

    agregando nav a la vista home

--- apps/frontend/src/pages/home/index.tsx
+++ apps/frontend/src/pages/home/index.tsx
@@ -1,5 +1,14 @@
+import { NavBar } from "@common/components/navbar/navbar";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/home/")({
-  component: () => <div>Hello /home/!</div>,
+  component: () => Home(),
 });
+
+export function Home() {
+  return (
+    <main>
+      <NavBar />
+    </main>
+  );
+}
