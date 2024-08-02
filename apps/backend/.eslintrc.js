30e7182beae1393c9d87235787b39dadde93d3a6
JFernando13 <fernando.juan.0613@gmail.com>
2024-08-02 17:50:10 -0500

    style(backend): :art: mejorando las reglas de eslint
    Añadiendo reglas para mejorar estandarizar la manera en la que se escribe el codigo.

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -5,10 +5,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier', 'jsdoc'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    // 'plugin:prettier/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jsdoc/recommended',
   ],
   root: true,
   env: {
@@ -21,5 +22,33 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        semi: true,
+        arrowParens: 'always',
+        proseWrap: 'always',
+        bracketSameLine: true,
+      },
+    ],
+    'jsdoc/require-jsdoc': [
+      'error',
+      {
+        require: {
+          ClassExpression: true,
+          ClassDeclaration: true,
+          FunctionExpression: true,
+          ArrowFunctionExpression: true,
+          FunctionDeclaration: true,
+          MethodDefinition: true,
+        },
+        checkConstructors: false,
+      },
+    ],
+    'jsdoc/check-line-alignment': 'error',
   },
 };
