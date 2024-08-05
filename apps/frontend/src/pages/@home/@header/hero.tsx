4508913a32f2d1c9f419ad606d2fb68dab00091b
CardManj <c.manjarres2003@gmail.com>
2024-08-05 14:51:40 -0500

    feat(frontend): :sparkles: Agregando video principal al home
    Se agrega el video principal usando ReactPlayer y se incorporan funcionalidades

--- apps/frontend/src/pages/@home/@header/hero.tsx
+++ apps/frontend/src/pages/@home/@header/hero.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/common/components/buttons";
 import { ButtonColor, ButtonSize, ButtonVariant } from "@/common/components/buttons/button";
@@ -7,24 +8,37 @@ import { synopsis_group } from "@/common/services/data";
 
 export function Hero() {
   const navigate = useNavigate();
+  const [hide, setHide] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setHide(true);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <section className="flex h-screen w-[80%] items-end justify-start max-lg:w-full max-lg:py-16">
-      <div className="space-y-8 px-4 max-md:py-5 lg:py-40">
+    <section className="relative z-10 flex h-screen w-full items-end justify-start p-4 pb-24">
+      <div className="space-y-8 px-4 py-10">
         {synopsis_group.map((synopsis) => (
           <hgroup
             key={synopsis.description}
-            className="grid gap-2 px-7 text-left max-lg:px-20 max-sm:px-0"
+            className={`grid transform gap-2 text-left transition-all duration-1000 ease-in-out ${hide ? "translate-y-4" : "translate-y-0"} max-lg:px-5 max-sm:px-0`}
           >
             <h3 className="text-pretty font-poppins text-7xl font-bold max-lg:text-6xl max-sm:text-5xl">
               {synopsis.title}
             </h3>
-            <p className="w-[50%] text-lg max-xl:w-full max-sm:text-base">{synopsis.description}</p>
+            <p
+              className={`w-[50%] text-lg transition-opacity duration-1000 ease-in-out max-xl:w-full max-sm:text-base ${hide ? "hidden opacity-0" : "opacity-100"}`}
+            >
+              {synopsis.description}
+            </p>
           </hgroup>
         ))}
-        <div className="flex flex-row gap-2 px-7 max-lg:px-20 max-sm:w-[70%] max-sm:px-0 max-[556px]:w-full max-[556px]:flex-col max-[556px]:items-center">
+        <div className="flex flex-row gap-2 max-sm:w-[70%] max-sm:px-0 max-[556px]:w-full max-[556px]:flex-col max-[556px]:items-center">
           <Button
-            className="flex items-center gap-2 max-sm:text-sm max-[556px]:px-11"
+            className="flex items-center gap-2 max-sm:text-sm"
             color={ButtonColor.Gold}
             size={ButtonSize.Small}
             variant={ButtonVariant.Standard}
@@ -33,7 +47,7 @@ export function Hero() {
             <PlayIcon className="m-1 w-4" />
             Reproducir
           </Button>
-          <button className="flex items-center gap-2 rounded-md border-none bg-[#a2a1a1c7] p-2 text-white/75 hover:bg-[#c1c1c1c7] max-sm:text-sm">
+          <button className="flex items-center gap-2 rounded-md border-none bg-[#a2a1a1b4] p-2 text-white/75 hover:bg-[#c1c1c1c7] max-sm:text-sm">
             <InfoIcon className="m-1 w-6" />
             Más información
           </button>
--- apps/frontend/src/pages/@home/@header/index.tsx
+++ apps/frontend/src/pages/@home/@header/index.tsx
@@ -1,13 +1,49 @@
+import { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+
+import video from "../../../../public/assets/video.mp4";
+
 import { Hero } from "./hero";
 
 import NavBar from "@/common/components/navbar/navbar";
 
 export function Header() {
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setPlaying(true);
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <section className="relative h-screen border-b-4 border-y-zinc-800 bg-black/70 bg-banner-hero bg-cover bg-center bg-no-repeat bg-blend-multiply [animation-range:exit] [animation-timeline:view()]">
+    <section className="relative h-screen border-b-4 border-y-zinc-800 bg-black/70">
       <header className="relative z-10 flex h-full flex-col justify-between">
         <NavBar />
         <Hero />
+        <ReactPlayer
+          loop
+          className="z-0"
+          config={{
+            file: {
+              attributes: {
+                style: {
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  objectPosition: "center",
+                },
+              },
+            },
+          }}
+          height="100%"
+          playing={playing}
+          style={{ position: "absolute", top: 0, left: 0 }}
+          url={video}
+          width="100%"
+        />
       </header>
     </section>
   );
